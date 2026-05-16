2b035f516f73ef47916623ec604df046d7a27b59
emanoel <eoemanoel@gmail.com>
2026-05-16 15:57:39 -0300

    feat: criar rotas e páginas base

---
+++ src/app/pages/player-profile/player-profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'PlayerProfileComponent',
+  imports: [],
+  templateUrl: './player-profile.html',
+  styleUrl: './player-profile.css',
+})
+export class PlayerProfileComponent {}
