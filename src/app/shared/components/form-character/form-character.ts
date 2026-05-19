0daff6431cd789435f76b41875417e7912474983
Igor Andrade <igor.andgalv123@gmail.com>
2026-05-19 14:33:58 -0300

    Feature: Form-character created

---
+++ src/app/shared/components/form-character/form-character.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-character',
+  imports: [],
+  templateUrl: './form-character.html',
+  styleUrl: './form-character.css',
+})
+export class FormCharacter {}
